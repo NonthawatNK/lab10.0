1f3afd9cd178cc266468356cda830134a00582b0
NonthawatNK <Nonthawt20@gmail.com>
2021-01-12 16:38:10 +0700

    อ่าาาาาาาาาเสร็จแล้ว

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,16 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+int main (){
+    string g[9]={"A","B+","B","C+","C","D+","D","F","W"};
+    cout <<"Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int x =rand()%9;
+    cout <<"You will get "<< g[x] <<" in this 261102.";
+    return 0;
+}
